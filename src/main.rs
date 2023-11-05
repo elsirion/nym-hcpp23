dd861aa76aa1bfb0af20c4f189cdc9429eec58c2
elsirion <elsirion@protonmail.com>
2023-11-05 14:21:29 +0100

    fix: leave client running to avoid errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use nym_sdk::mixnet::{MixnetClient, MixnetMessageSender, Recipient};
 use clap::Parser;
 
@@ -12,22 +13,29 @@ enum Opts {
 #[tokio::main]
 async fn main() {
     let opts: Opts = Parser::parse();
-    nym_bin_common::logging::setup_logging();
 
+    // For debugging uncomment the following line
+    // nym_bin_common::logging::setup_logging();
+
+    println!("Starting Nym client ...");
     let mut nym_client = MixnetClient::connect_new().await.expect("Could not build Nym client");
 
     match opts {
         Opts::Client { recipient } => {
-            let mut msg = String::new();
-            // std::io::stdin().read_line(&mut msg).expect("Stdin read failed");
-            nym_client.send_plain_message(recipient, "message").await.expect("Nym send failed");
+            loop {
+                print!("Write: ");
+                std::io::stdout().flush().expect("Failed to flush stdout");
+                let mut msg = String::new();
+                std::io::stdin().read_line(&mut msg).expect("Stdin read failed");
+                nym_client.send_plain_message(recipient, msg.trim()).await.expect("Nym send failed");
+            }
         },
         Opts::Server => {
             println!("Nym address: {}", nym_client.nym_address());
             loop {
                 let Some(msgs) = nym_client.wait_for_messages().await else { continue };
                 for msg in msgs {
-                    println!("{}", String::from_utf8_lossy(&msg.message));
+                    println!("Received: {}", String::from_utf8_lossy(&msg.message));
                 }
             }
         }
